41e3ea4a029abaa6a9a4914d15e62098e87dba0e
CyTon Code <cytoncode@gmail.com>
2021-08-18 00:01:53 +0000

    Update container.hpp

--- lib/container.hpp
+++ lib/container.hpp
@@ -1,28 +1,28 @@
 /*
- * File: container.hpp
- * Language: Cpp
- * Author: Dal Bo Yob
- * We: DBY Company
- *
- * Copyright 2021 DBY Company <dal@bo.yob>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- *
- *
- */
+* File: container.hpp
+* Language: Cpp
+* Author: Dal Bo Yob
+* We: DBY Company
+*
+* Copyright 2021 DBY Company <dal@bo.yob>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+*
+*
+*/
 
 
 #ifndef GAME_OBJECT_NOT__TANGIBLE_CONTAINER
@@ -32,32 +32,31 @@
 
 class Container : public NotTangible
 {
-    /* Example Container: Cell Slot Panel ... */
+  /* Example Container: Cell Slot Panel ... */
 
-    public:
+  public:
 
-        Container()
-        {
-            this->add_my_type("Container");
+    Container()
+    {
+      this->add_my_type("Container");
 
-            cout << "Container was created" << endl;
-        }
+      cout << "Container was created" << endl;
+    }
 
-        ~Container()
-        {
+    ~Container()
+    {
+      cout << "Container was killed" << endl;
+    }
 
-            cout << "Container was killed" << endl;
-        }
+    void clearContainer()
+    {
+      this->my_res.clear();
+    }
 
-        void clearContainer()
-        {
-            this->my_res.clear();
-        }
+  private:
 
-    private:
-
-        // ресурсы которые я в себе храню
-        std::vector<std::string> my_res;
+    // ресурсы которые я в себе храню
+    std::vector<std::string> my_res;
 };
 
 #endif // GAME_OBJECT_NOT__TANGIBLE_CONTAINER
